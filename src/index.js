252f508209c6293c0808e48e60f32c9c49b95ba5
DzmitrySudalenka <sudalenko@gmail.com>
2023-01-19 18:44:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,27 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const DIGITS_TABLE = {
+  '00': '',
+  '10': '.',
+  '11': '-'
+};
+
 function decode(expr) {
-    // write your solution here
+  let result = '';
+  for (let i = 0; i < expr.length; i += 10) {
+    const digitalSymbol = expr.slice(i, i + 10);
+    if (digitalSymbol === '**********') {
+      result += ' ';
+    } else {
+      let morseSymbol = '';
+      for (let j = 0; j < digitalSymbol.length; j += 2) {
+        morseSymbol += DIGITS_TABLE[digitalSymbol.slice(j, j + 2)];
+      }
+      result += MORSE_TABLE[morseSymbol];
+    }
+  }
+  return result;
 }
 
 module.exports = {
